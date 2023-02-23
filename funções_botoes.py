3f9230b95a57be2d52b91a70162765d5f837f48c
M1theusGM <matheusguedes_m@hotmail.com>
2023-02-23 11:12:45 -0300

    commit inicial

---
+++ funções_botoes.py
@@ -0,0 +1,60 @@
+def valor_botao_0():
+    print('0')
+
+def valor_botao_1():
+    print('1')
+
+def valor_botao_2():
+    print('2')
+    
+def valor_botao_3():
+    print('3')
+
+def valor_botao_4():
+    print('4')
+
+def valor_botao_5():
+    print('5')
+
+def valor_botao_6():
+    print('6')
+
+def valor_botao_7():
+    print('7')
+
+def valor_botao_8():
+    print('8')   
+    
+def valor_botao_9():
+    print('9')
+
+def virgula():
+    print('.')
+
+def igual():
+    print('=')
+
+def soma():
+    print('+')
+
+def subtra():
+    print('-')
+
+def multipli():
+    print('x')
+
+def divisao():
+    print('÷')
+
+def limpar():
+    print('CE')
+
+def ao_quadrado():
+    print('x²')
+
+def raiz_quadrada():
+    print('²√x')
+
+def limpar_um_por_um():
+    print('⌫')
+
